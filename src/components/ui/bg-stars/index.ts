953a6808eb44cbf502c02ef56415c66ea316ea32
weiwei <seabree12@gmail.com>
2025-09-14 23:03:07 +0800

    Clean up redundant vue pages

--- src/components/ui/bg-stars/index.ts
+++ src/components/ui/bg-stars/index.ts
@@ -1,2 +1,1 @@
 export { default as StarsBackground } from "./StarsBackground.vue";
-export { default as EnhancedStarsBackground } from "./EnhancedStarsBackground.vue";
--- src/components/ui/cosmic-portal-simple/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CosmicPortalSimple } from "./CosmicPortalSimple.vue";
